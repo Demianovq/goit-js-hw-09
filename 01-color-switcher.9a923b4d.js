ba4af85594f948eb1543e764ae70b56cc4c8b121
Demianovq <110334010+Demianovq@users.noreply.github.com>
2023-01-08 12:34:50 +0000

    Deploying to gh-pages from @ Demianovq/goit-js-hw-09@505115e8a3df9002581b9e1807922f962c9820fd 🚀

---
+++ 01-color-switcher.9a923b4d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(function(){d=setInterval((()=>{const d=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=d,t.disabled=!0,e.disabled=!1}),1e3)})),e.disabled=!0,e.addEventListener("click",(()=>{clearInterval(d),e.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9a923b4d.js.map
